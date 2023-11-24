dd82751e58670b2b68161ade1bd781e7cb1ee2d4
Xav <54720357+MisterXav@users.noreply.github.com>
2023-11-24 23:13:26 +0100

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\owenc\\Wiki\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\owenc\\Wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\owenc\\Wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\owenc\\Wiki\\src\\css\\custom.css"),
-];
