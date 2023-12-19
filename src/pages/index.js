8cb6b4b74be3e2017967132f07e87fe56bbd8daf
MisterXav5 <valckbretonxavier@gmail.com>
2023-12-19 22:27:56 +0100

    fix: Lien bouton page accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/category/commencer-en-5-minutes">
             Commencer a jouer ⏱️
           </Link>
         </div>
