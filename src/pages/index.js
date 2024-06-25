098f8d7e04a83d8b94f6126bd75a8529709b9646
JVscript <JVscript>
2024-06-25 12:18:15 +0200

    feat(homepage): 1ere partie accueil Video + bouton commencer l'aventure

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,30 +4,39 @@ import Layout from '@theme/Layout';
 function HomepageHeader() {
   return (
 
-    <section class="bg-center bg-no-repeat bg-[url('/static/img/atlasbackground.png')] bg-gray-700 bg-blend-multiply">
-      <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"></p>
-        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
 
-          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img class="rounded-t-lg" src="/static/img/atlasbackground.png" alt="" />
-            </a>
-            <div class="p-5">
-              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">AtlasNation</h1>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-200">C'est vraiment cool</p>
-              <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-              </a>
-            </div>
-          </div>
+<section class="bg-center bg-cover bg-no-repeat bg-[url('/static/img/atlasbackground.png')] bg-gray-700 bg-blend-multiply">
 
-        </div>
+
+  <div class="px-4 mx-auto max-w-screen-xl text-center py-0">
+
+    <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"></p>
+    
+    <video class="px-auto mx-auto max-w-screen border-1 border-gray-200 rounded-lg shadow py-0 lg:py-0" alt="" playsinline autoplay="autoplay" loop="loop" muted>
+      <source src={require('/home.mp4').default} type="video/mp4"/>
+    </video>
+
+    <div class=" space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+
+      <div class="p-5">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">AtlasNation</h1>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-200">Bienvenue</p>
+        <a href="#joinserver" class="group/hoveranimation no-underline hover:no-underline inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <svg class="transition transform group-hover/hoveranimation:translate-y-0.5 motion-reduce:transition-none motion-reduce:hover:transform-none mx-1 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+          </svg>
+          Commencer l'aventure
+          <svg class="transition transform group-hover/hoveranimation:translate-y-0.5 motion-reduce:transition-none motion-reduce:hover:transform-none mx-1 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+          </svg>
+
+        </a>
+
       </div>
-    </section>
+
+    </div>
+  </div>
+</section>
   );
 }
 
