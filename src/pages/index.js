659857d104049f46d4c53833d0025fbe2df4b9c0
JVscript <JVscript>
2024-06-30 17:52:02 +0200

    feat: section "invitation a jouer, a regarder la dynmap et la doc"

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function HomepageHeader() {
     </a>
     </div>
 </section>
-<section id="joinserver">
+<section id="joinserver" class="mx-2">
 
   <h2 class="text-4xl font-semibold">Commencer l'aventure</h2>
 
@@ -92,6 +92,39 @@ function HomepageHeader() {
       </div>
     </div>
 
+</section>
+<section class="mx-2">
+  <h2 class="text-4xl font-semibold">Liens utiles</h2>
+  <div class="grid grid-cols-2 gap-4 mt-5">
+
+
+    <div class=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="http://map.atlasnation.fr/">
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Map dynamique</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Prenez de la hauteur, et observez le monde en temps réel, depuis le ciel !</p>
+        <a href="http://map.atlasnation.fr/" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+            Ouvrir la dynmap
+            <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+            </svg>
+        </a>
+    </div>
+
+    <div class=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="http://docs.atlasnation.fr/">
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Documentation</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Vous retrouverez ici toutes les informations concernant le serveur, afin de profiter de la meilleure expérience possible.</p>
+        <a href="http://docs.atlasnation.fr/" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+            Ouvrir la documentation
+            <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+            </svg>
+        </a>
+    </div>
+
+  </div>
 </section>
 </div>
 
